0eaff521ebec180500074a85ee27857f9891c85f
SpiritOfSim <spiritofsim@yandex.ru>
2018-10-06 15:19:19 +0300

    Added eval func stub

---
+++ evaluator.go
@@ -0,0 +1,9 @@
+package handeval
+
+// Eval evaluates poker hand
+// Can evaluate 5,6 and 7 hands
+// Rank is the weight of the hand. Bigger rank is best hand
+// HandType is the name of combination
+func Eval(c Card) (rank uint32, handType HandType) {
+	return 1, 1
+}
